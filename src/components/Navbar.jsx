a4250ac62b9993672059a4923c3211f4f49cd6c0
Alejandro <alejandroidb@gmail.com>
2024-07-19 20:41:38 -0400

    login function logic established

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,46 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loguear = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://codigo-alfa.cl/bootcamp-socius2024/Api/loginUser",
+        {
+          method: "POST",
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error en la solicitud");
+      }
+
+      const data = await response.json();
+
+      if (data.success) {
+        navigate("/Curriculum")
+        setEmail('');
+        setPassword('')
+      } else {
+        alert("Correo o contraseña incorrectos");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Hubo un problema con la autenticación");
+    }
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
-          <NavLink className="navbar-brand text-light" to='/'>
+          <NavLink className="navbar-brand text-light" to="/">
             TareaAdmin
           </NavLink>
           <button
@@ -22,11 +56,11 @@ const Navbar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+              <li className="nav-item">
                 <NavLink
                   className="nav-link active text-light"
                   aria-current="page"
-                 to='/Curriculum'
+                  to="/Curriculum"
                 >
                   Cv
                 </NavLink>
@@ -46,18 +80,24 @@ const Navbar = () => {
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
+            <form className="d-flex" role="search" onSubmit={loguear}>
               <input
                 className="form-control me-2"
                 type="email"
                 placeholder="Ingrese su correo"
                 aria-label="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
               <input
                 className="form-control me-2"
                 type="password"
                 placeholder="Ingrese su contraseña"
                 aria-label="contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
               />
               <button
                 className="btn btn-outline-success bg-success text-light"
